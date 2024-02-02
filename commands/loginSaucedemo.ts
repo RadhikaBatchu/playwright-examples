805c10a967d381aeaba6dec641687ff1599dcfe9
Aleksa Jovicic <aleksa.j@outlook.com>
2024-02-02 09:42:03 +0100

    Organise tests

--- commands/loginSaucedemo.ts
+++ commands/loginSaucedemo.ts
@@ -9,16 +9,12 @@ export const loginSaucedemo = async (page: Page, params: Params) => {
   const username = params.username;
   const password = params.password;
 
-  // Also would be nice to put it in an .env variable for easier configuration
-  // when running on ci or targeting specific environments
-  await page.goto("https://saucedemo.com");
-
-  const usernameInput = page.locator('[data-test="username"]');
-  const passwordInput = page.locator('[data-test="password"]');
+  const usernameInput = page.getByTestId("username");
+  const passwordInput = page.getByTestId("password");
 
   await usernameInput.fill(username);
   await passwordInput.fill(password);
 
-  const signInButton = page.locator('[data-test="login-button"]');
+  const signInButton = page.getByTestId("login-button");
   await signInButton.click();
 };
